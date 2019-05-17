6f78651514bfdcbcc53c895d6fb3ba9cb1bda185
SangChan Lee <sangchan82@gmail.com>
2019-05-17 11:25:12 +0900

    new one

--- 20190515.py
+++ 20190515.py
@@ -2,3 +2,19 @@ def testfunc(myname) :
     print("Hello %s" %myname)
 
 print("Sunshine")
+
+another = 100
+def variable_test() :
+    first = 10
+    second = 20
+    return first * second * another
+
+print(variable_test())
+
+import time
+
+print(time.asctime())
+
+import sys
+
+print(sys.stdin.readline())

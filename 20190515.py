d4448a72f6bf85773e5d23788dd754824c728f65
SangChan Lee <sangchan82@gmail.com>
2019-05-18 08:02:52 +0900

    add new

--- 20190515.py
+++ 20190515.py
@@ -18,3 +18,24 @@ print(time.asctime())
 import sys
 
 print(sys.stdin.readline())
+
+class Things:
+    pass
+
+class Inanimate(Things):
+    pass
+
+class Animate(Things):
+    pass
+
+class Sidewalks(Inanimate):
+    pass
+
+class Animals(Animate):
+    pass
+
+class Mammals(Animals):
+    pass
+
+class Giraffes(Mammals):
+    pass

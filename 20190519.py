d914c30efd618504c5c105f57785d37d0e3e7759
SangChan Lee <sangchan82@gmail.com>
2019-05-20 14:01:50 +0900

    add new

---
+++ 20190519.py
@@ -0,0 +1,14 @@
+print(abs(10))
+print(abs(-10))
+
+print(bool(0))
+print(bool(1))
+print(bool(None))
+print(bool('a'))
+print(bool(' '))
+print(bool(''))
+
+my_list = []
+print(bool(my_list))
+my_list.append("s")
+print(bool(my_list))

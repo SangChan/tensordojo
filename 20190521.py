855002c630e84733409e70d847c97292aa37644a
SangChan Lee <sangchan82@gmail.com>
2019-05-24 14:14:43 +0900

    random test

--- 20190521.py
+++ 20190521.py
@@ -20,3 +20,20 @@ import keyword
 print(keyword.iskeyword('if'))
 print(keyword.iskeyword('ozwald'))
 print(keyword.kwlist)
+
+import random
+
+print(random.randint(1,100))
+num = random.randint(1,100)
+
+while True:
+    print('guess a number between 1 and 100')
+    guess = input()
+    i = int(guess)
+    if i == num :
+        print('you guessed right')
+        break
+    elif i < num :
+        print('try higher')
+    elif i > num :
+        print('try lower')

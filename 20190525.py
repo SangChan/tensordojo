b0061917c91cc5c60e443830104c17fd7124acd2
SangChan Lee <sangchan82@gmail.com>
2019-05-27 14:22:27 +0900

    add another turtle example

--- 20190525.py
+++ 20190525.py
@@ -3,6 +3,24 @@ import turtle
 t = turtle.Pen()
 
 t.reset()
-for x in range(1,5):
-    t.forward(50)
-    t.left(90)
+for x in range(1,9):
+    t.forward(100)
+    t.left(225)
+
+t.reset()
+for x in range(1,38):
+    t.forward(100)
+    t.left(175)
+
+t.reset()
+for x in range(1,20):
+    t.forward(100)
+    t.left(95)
+
+t. reset()
+for x in range(1,19):
+    t.forward(100)
+    if x % 2 == 0 :
+        t.left(175)
+    else :
+        t.left(225)

b4f73aeb2411dc5ee83cea6566b388791fadeb66
SangChan Lee <sangchan82@gmail.com>
2019-06-01 14:21:17 +0900

    canvas test

--- 20190528.py
+++ 20190528.py
@@ -4,8 +4,13 @@ def hello():
     print("hello there")
 
 
-tk = tkinter.Tk()
-btn = tkinter.Button(tk, text = "click me", command=hello)
-btn.pack()
-tkinter.mainloop()
+#tk = tkinter.Tk()
+#btn = tkinter.Button(tk, text = "click me", command=hello)
+#btn.pack()
+#tkinter.mainloop()
 
+tk = tkinter.Tk()
+canvas = tkinter.Canvas(tk, width = 500, height = 500)
+canvas.pack()
+canvas.create_line(0,0,500,500)
+tkinter.mainloop()

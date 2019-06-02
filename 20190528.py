6b5a79cbb04d33f6759a64762cbc630c675ea984
SangChan Lee <sangchan82@gmail.com>
2019-06-02 21:08:14 +0900

    random drawing

--- 20190528.py
+++ 20190528.py
@@ -1,4 +1,5 @@
 import tkinter
+import random
 
 def hello():
     print("hello there")
@@ -9,8 +10,17 @@ def hello():
 #btn.pack()
 #tkinter.mainloop()
 
+def random_rect(width, height):
+    x1 = random.randrange(width)
+    y1 = random.randrange(height)
+    x2 = x1 + random.randrange(width)
+    y2 = y1 + random.randrange(height)
+    canvas.create_rectangle(x1,y1,x2,y2)
+
 tk = tkinter.Tk()
 canvas = tkinter.Canvas(tk, width = 500, height = 500)
 canvas.pack()
-canvas.create_line(0,0,500,500)
+#canvas.create_line(0,0,500,500)
+for x in range(0,100):
+    random_rect(400,400)
 tkinter.mainloop()

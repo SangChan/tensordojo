36af10f8977ee20d19dd3817af912fc690ed37c6
SangChan Lee <sangchan82@gmail.com>
2019-06-07 21:26:50 +0900

    create ball on middle

--- 20190605.py
+++ 20190605.py
@@ -2,6 +2,15 @@ import tkinter
 import random
 import time
 
+class Ball:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_oval(10,10,25,25,fill=color)
+        self.canvas.move(self.id, 245,100)
+
+    def draw(self):
+        pass
+
 tk = tkinter.Tk()
 tk.title("Game")
 tk.resizable(0,0)
@@ -9,3 +18,9 @@ tk.wm_attributes("-topmost",1)
 canvas = tkinter.Canvas(tk, width=500, height=400, bd = 0, highlightthickness=0)
 canvas.pack()
 tk.update()
+ball = Ball(canvas, 'red')
+
+while 1:
+    tk.update_idletasks()
+    tk.update()
+    time.sleep(0.01)

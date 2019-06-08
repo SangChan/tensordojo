665cc9eaf80c74e00e815f5442a4b56750920587
SangChan Lee <sangchan82@gmail.com>
2019-06-08 21:30:38 +0900

    bound on vertical

--- 20190605.py
+++ 20190605.py
@@ -7,9 +7,17 @@ class Ball:
         self.canvas = canvas
         self.id = canvas.create_oval(10,10,25,25,fill=color)
         self.canvas.move(self.id, 245,100)
+        self.x = 0
+        self.y = -1
+        self.canvas_height = self.canvas.winfo_height()
 
     def draw(self):
-        pass
+        self.canvas.move(self.id,self.x,self.y)
+        pos = self.canvas.coords(self.id)
+        if pos[1] <= 0:
+            self.y = 1
+        if pos[3] >= self.canvas_height:
+            self.y = -1
 
 tk = tkinter.Tk()
 tk.title("Game")
@@ -21,6 +29,7 @@ tk.update()
 ball = Ball(canvas, 'red')
 
 while 1:
+    ball.draw()
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)

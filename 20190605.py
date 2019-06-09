550c6ed538b97f512a8f7b6541448eb5469feb98
SangChan Lee <sangchan82@gmail.com>
2019-06-09 21:35:47 +0900

    draw paddle

--- 20190605.py
+++ 20190605.py
@@ -28,6 +28,15 @@ class Ball:
         if pos[2] >= self.canvas_width:
             self.x = -1
 
+class Paddle:
+    def __init__(self,canvas,color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0,0,100,10,fill=color)
+        self.canvas.move(self.id, 200, 300)
+
+    def draw(self):
+        pass
+
 tk = tkinter.Tk()
 tk.title("Game")
 tk.resizable(0,0)
@@ -35,10 +44,12 @@ tk.wm_attributes("-topmost",1)
 canvas = tkinter.Canvas(tk, width=500, height=400, bd = 0, highlightthickness=0)
 canvas.pack()
 tk.update()
+paddle = Paddle(canvas, 'blue')
 ball = Ball(canvas, 'red')
 
 while 1:
     ball.draw()
+    paddle.draw()
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)

1e4a033d89a0b6b5bdf111ee7fe4245d4a34b0ac
Sangchan Lee <sangchan82@gmail.com>
2019-06-14 08:48:43 +0900

    debug paddle game

--- 20190605.py
+++ 20190605.py
@@ -45,9 +45,9 @@ class Paddle:
         self.id = canvas.create_rectangle(0,0,100,10,fill=color)
         self.canvas.move(self.id, 200, 300)
         self.x = 0
-        self.canvas_width = self.canvas_winfo_width()
+        self.canvas_width = self.canvas.winfo_width()
         self.canvas.bind_all("<KeyPress-Left>", self.turn_left)
-        self.canvas.bind_all("<KeyPress_Right", self.turn_right)
+        self.canvas.bind_all("<KeyPress-Right>", self.turn_right)
 
     def turn_left(self, evt):
         self.x = -2

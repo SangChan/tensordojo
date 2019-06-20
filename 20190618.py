77f49c7dc6d852699a05b9d95fe7262b2af2084a
Sangchan Lee <sangchan82@gmail.com>
2019-06-20 16:21:10 +0900

    complete class Game

--- 20190618.py
+++ 20190618.py
@@ -5,3 +5,11 @@ import time
 class Game:
     def __init__(self):
         self.tk = Tk()
+        self.tk.title("Mr.stick man races for exit")
+        self.tk.resizable(0,0)
+        self.tk.wm_attribute("-topmost",1)
+        self.canvas = Canvas(self.tk, width = 500, height = 500, highlightthickness=0)
+        self.canvas.pack()
+        self.tk.update()
+        self.canvas_height = 500
+        self.canvas_width = 500

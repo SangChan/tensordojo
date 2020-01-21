10c0dd50a4dfe8c27fa5dd46c56a22fc3e1a9d9e
Sangchan <sangchan82@gmail.com>
2020-01-21 17:22:51 +0900

    add new

--- 20200115.py
+++ 20200115.py
@@ -6,6 +6,8 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
+print(tf.__version__)
+
 fashion_mnist = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 

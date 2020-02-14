327251a30f98f437f50b741d506f96d3da03dba9
Sangchan <sangchan82@gmail.com>
2020-02-14 15:51:16 +0900

    copy code for testing another machine

--- 20200209.py
+++ 20200209.py
@@ -1,3 +1,46 @@
 # 20200209.py
+from __future__ import absolute_import, division, print_function, unicode_literals, unicode_literals
 
 import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+NUM_WORDS = 10000
+
+(train_data, train_labels), (test_data, test_labels) = keras.datasets.imdb.load_data(num_words=NUM_WORDS)
+
+def multi_hot_sequences(sequences, dimension):
+    # 0으로 채워진 (len(sequences), dimension) 크기의 행렬을 만듭니다
+    results = np.zeros((len(sequences), dimension))
+    for i, word_indices in enumerate(sequences):
+        results[i, word_indices] = 1.0  # results[i]의 특정 인덱스만 1로 설정합니다
+    return results
+
+
+train_data = multi_hot_sequences(train_data, dimension=NUM_WORDS)
+test_data = multi_hot_sequences(test_data, dimension=NUM_WORDS)
+plt.plot(train_data[0])
+
+baseline_model = keras.Sequential([
+    # `.summary` 메서드 때문에 `input_shape`가 필요합니다
+    keras.layers.Dense(16, activation='relu', input_shape=(NUM_WORDS,)),
+    keras.layers.Dense(16, activation='relu'),
+    keras.layers.Dense(1, activation='sigmoid')
+])
+
+baseline_model.compile(optimizer='adam',
+                       loss='binary_crossentropy',
+                       metrics=['accuracy', 'binary_crossentropy'])
+
+baseline_model.summary()
+
+baseline_history = baseline_model.fit(train_data,
+                                      train_labels,
+                                      epochs=20,
+                                      batch_size=512,
+                                      validation_data=(test_data, test_labels),
+                                      verbose=2)

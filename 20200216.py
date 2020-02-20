6edaf9d5788b33631ba88312d079f6adcacf2e1e
Sangchan <sangchan82@gmail.com>
2020-02-20 14:51:54 +0900

    put code from jnotebook for testing time to spend

--- 20200216.py
+++ 20200216.py
@@ -1,1 +1,129 @@
-# new file!!
+from __future__ import absolute_import, division, print_function, unicode_literals, unicode_literals
+
+import os
+
+import tensorflow as tf
+from tensorflow import keras
+
+tf.__version__
+
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+
+train_labels = train_labels[:1000]
+test_labels = test_labels[:1000]
+
+train_images = train_images[:1000].reshape(-1, 28 * 28) / 255.0
+test_images = test_images[:1000].reshape(-1, 28 * 28) / 255.0
+
+# 간단한 Sequential 모델을 반환합니다
+def create_model():
+  model = tf.keras.models.Sequential([
+    keras.layers.Dense(512, activation='relu', input_shape=(784,)),
+    keras.layers.Dropout(0.2),
+    keras.layers.Dense(10, activation='softmax')
+  ])
+
+  model.compile(optimizer='adam',
+                loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
+
+  return model
+
+
+# 모델 객체를 만듭니다
+model = create_model()
+model.summary()
+
+checkpoint_path = "training_1/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# 체크포인트 콜백 만들기
+cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+
+model = create_model()
+
+model.fit(train_images, train_labels,  epochs = 10,
+          validation_data = (test_images,test_labels),
+          callbacks = [cp_callback])  # 훈련 단계에 콜백을 전달합니다
+
+# 옵티마이저의 상태를 저장하는 것과 관련되어 경고가 발생할 수 있습니다.
+# 이 경고는 (그리고 이 노트북의 다른 비슷한 경고는) 이전 사용 방식을 권장하지 않기 위함이며 무시해도 좋습니다.
+
+!ls {checkpoint_dir}
+
+model = create_model()
+
+loss, acc = model.evaluate(test_images,  test_labels, verbose=2)
+print("훈련되지 않은 모델의 정확도: {:5.2f}%".format(100*acc))
+
+model.load_weights(checkpoint_path)
+loss,acc = model.evaluate(test_images,  test_labels, verbose=2)
+print("복원된 모델의 정확도: {:5.2f}%".format(100*acc))
+
+# 파일 이름에 에포크 번호를 포함시킵니다(`str.format` 포맷)
+checkpoint_path = "training_2/cp-{epoch:04d}.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+cp_callback = tf.keras.callbacks.ModelCheckpoint(
+    checkpoint_path, verbose=1, save_weights_only=True,
+    # 다섯 번째 에포크마다 가중치를 저장합니다
+    period=5)
+
+model = create_model()
+model.save_weights(checkpoint_path.format(epoch=0))
+model.fit(train_images, train_labels,
+          epochs = 50, callbacks = [cp_callback],
+          validation_data = (test_images,test_labels),
+          verbose=0)
+
+! ls {checkpoint_dir}
+
+latest = tf.train.latest_checkpoint(checkpoint_dir)
+latest
+
+model = create_model()
+model.load_weights(latest)
+loss, acc = model.evaluate(test_images,  test_labels, verbose=2)
+print("복원된 모델의 정확도: {:5.2f}%".format(100*acc))
+
+# 가중치를 저장합니다
+model.save_weights('./checkpoints/my_checkpoint')
+
+# 가중치를 복원합니다
+model = create_model()
+model.load_weights('./checkpoints/my_checkpoint')
+
+loss,acc = model.evaluate(test_images,  test_labels, verbose=2)
+print("복원된 모델의 정확도: {:5.2f}%".format(100*acc))
+
+model = create_model()
+
+model.fit(train_images, train_labels, epochs=5)
+
+# 전체 모델을 HDF5 파일로 저장합니다
+model.save('my_model.h5')
+
+# 가중치와 옵티마이저를 포함하여 정확히 동일한 모델을 다시 생성합니다
+new_model = keras.models.load_model('my_model.h5')
+new_model.summary()
+
+loss, acc = new_model.evaluate(test_images,  test_labels, verbose=2)
+print("복원된 모델의 정확도: {:5.2f}%".format(100*acc))
+
+model = create_model()
+
+model.fit(train_images, train_labels, epochs=5)
+
+import time
+saved_model_path = "./saved_models/{}".format(int(time.time()))
+
+tf.keras.experimental.export_saved_model(model, saved_model_path)
+saved_model_path
+
+!ls saved_models/
+
+new_model = tf.keras.experimental.load_from_saved_model(saved_model_path)
+new_model.summary()
+model.predict(test_images).shape

20a235d2efc54036237582fde0ab1f9f8fc832fe
Sangchan <sangchan82@gmail.com>
2020-03-06 17:41:18 +0900

    move code to py file

--- 20200302.py
+++ 20200302.py
@@ -1,3 +1,56 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
 
-print(tf.__version__)
+x = tf.ones((2, 2))
+
+with tf.GradientTape() as t:
+  t.watch(x)
+  y = tf.reduce_sum(x)
+  z = tf.multiply(y, y)
+
+# 입력 텐서 x에 대한 z의 도함수
+dz_dx = t.gradient(z, x)
+for i in [0, 1]:
+  for j in [0, 1]:
+    assert dz_dx[i][j].numpy() == 8.0
+
+x = tf.constant(3.0)
+with tf.GradientTape(persistent=True) as t:
+  t.watch(x)
+  y = x * x
+  z = y * y
+dz_dx = t.gradient(z, x)  # 108.0 (4*x^3 at x = 3)
+dy_dx = t.gradient(y, x)  # 6.0
+del t  # 테이프에 대한 참조를 삭제합니다.
+
+def f(x, y):
+  output = 1.0
+  for i in range(y):
+    if i > 1 and i < 5:
+      output = tf.multiply(output, x)
+  return output
+
+def grad(x, y):
+  with tf.GradientTape() as t:
+    t.watch(x)
+    out = f(x, y)
+  return t.gradient(out, x)
+
+x = tf.convert_to_tensor(2.0)
+
+assert grad(x, 6).numpy() == 12.0
+assert grad(x, 5).numpy() == 12.0
+assert grad(x, 4).numpy() == 4.0
+
+x = tf.Variable(1.0)  # 1.0으로 초기화된 텐서플로 변수를 생성합니다.
+
+with tf.GradientTape() as t:
+  with tf.GradientTape() as t2:
+    y = x * x * x
+  # 't' 컨텍스트 매니저 안의 그래디언트를 계산합니다.
+  # 이것은 또한 그래디언트 연산 자체도 미분가능하다는 것을 의미합니다. 
+  dy_dx = t2.gradient(y, x)
+d2y_dx2 = t.gradient(dy_dx, x)
+
+assert dy_dx.numpy() == 3.0
+assert d2y_dx2.numpy() == 6.0

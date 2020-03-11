a0ac651aff1d5e16fac1aaaa0564711237791fc3
Sangchan <sangchan82@gmail.com>
2020-03-11 18:11:01 +0900

    put code on py file

--- 20200307.py
+++ 20200307.py
@@ -1,1 +1,81 @@
-# new file for new move
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+# 파이썬 구문 사용
+x = tf.zeros([10, 10])
+x += 2  # 이것은 x = x + 2와 같으며, x의 초기값을 변경하지 않습니다.
+print(x)
+
+v = tf.Variable(1.0)
+assert v.numpy() == 1.0
+
+# 값을 재배열합니다.
+v.assign(3.0)
+assert v.numpy() == 3.0
+
+# tf.square()와 같은 텐서플로 연산에 `v`를 사용하고 재할당합니다. 
+v.assign(tf.square(v))
+assert v.numpy() == 9.0
+
+class Model(object):
+  def __init__(self):
+    # 변수를 (5.0, 0.0)으로 초기화 합니다.
+    # 실제로는 임의의 값으로 초기화 되어야합니다.
+    self.W = tf.Variable(5.0)
+    self.b = tf.Variable(0.0)
+
+  def __call__(self, x):
+    return self.W * x + self.b
+
+model = Model()
+
+assert model(3.0).numpy() == 15.0
+
+def loss(predicted_y, desired_y):
+  return tf.reduce_mean(tf.square(predicted_y - desired_y))
+
+TRUE_W = 3.0
+TRUE_b = 2.0
+NUM_EXAMPLES = 1000
+
+inputs  = tf.random.normal(shape=[NUM_EXAMPLES])
+noise   = tf.random.normal(shape=[NUM_EXAMPLES])
+outputs = inputs * TRUE_W + TRUE_b + noise
+
+import matplotlib.pyplot as plt
+
+plt.scatter(inputs, outputs, c='b')
+plt.scatter(inputs, model(inputs), c='r')
+plt.show()
+
+print('현재 손실: '),
+print(loss(model(inputs), outputs).numpy())
+
+def train(model, inputs, outputs, learning_rate):
+  with tf.GradientTape() as t:
+    current_loss = loss(model(inputs), outputs)
+  dW, db = t.gradient(current_loss, [model.W, model.b])
+  model.W.assign_sub(learning_rate * dW)
+  model.b.assign_sub(learning_rate * db)
+
+model = Model()
+
+# 도식화를 위해 W값과 b값의 변화를 저장합니다.
+Ws, bs = [], []
+epochs = range(10)
+for epoch in epochs:
+  Ws.append(model.W.numpy())
+  bs.append(model.b.numpy())
+  current_loss = loss(model(inputs), outputs)
+
+  train(model, inputs, outputs, learning_rate=0.1)
+  print('에포크 %2d: W=%1.2f b=%1.2f, 손실=%2.5f' %
+        (epoch, Ws[-1], bs[-1], current_loss))
+
+# 저장된 값들을 도식화합니다.
+plt.plot(epochs, Ws, 'r',
+         epochs, bs, 'b')
+plt.plot([TRUE_W] * len(epochs), 'r--',
+         [TRUE_b] * len(epochs), 'b--')
+plt.legend(['W', 'b', 'true W', 'true_b'])
+plt.show()

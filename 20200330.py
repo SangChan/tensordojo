88f8ca99fb06219f2ea868ba95213fc801f6d4ec
Sangchan <sangchan82@gmail.com>
2020-04-06 16:23:19 +0900

    remove file

--- 20200330.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-import tensorflow as tf
-from tensorflow import keras
-
-from tensorflow.keras import layers
--- 20200404.py
+++ 20200404.py
@@ -1,1 +1,6 @@
 # renamed from 20200330.py
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
+from tensorflow import keras
+
+from tensorflow.keras import layers

e4436d001c47fed6951d53d0c2834caf91f24675
Sangchan <sangchan82@gmail.com>
2020-04-08 17:45:09 +0900

    add new

--- 20200404.py
+++ 20200404.py
@@ -1,6 +1,8 @@
-# renamed from 20200330.py
 from __future__ import absolute_import, division, print_function, unicode_literals
-import tensorflow as tf
-from tensorflow import keras
 
-from tensorflow.keras import layers
+import os
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+print("텐서플로 버전: {}".format(tf.__version__))
+print("즉시 실행: {}".format(tf.executing_eagerly()))

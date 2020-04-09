aa99f89da2a13f7f85718a2280b7c0b4d9ab3c8c
SangChan Lee <sangchan82@gmail.com>
2020-04-09 21:05:33 +0900

    add new

--- 20200404.py
+++ 20200404.py
@@ -6,3 +6,10 @@ import tensorflow as tf
 
 print("텐서플로 버전: {}".format(tf.__version__))
 print("즉시 실행: {}".format(tf.executing_eagerly()))
+
+train_dataset_url = "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv"
+
+train_dataset_fp = tf.keras.utils.get_file(fname=os.path.basename(train_dataset_url),
+                                           origin=train_dataset_url)
+
+print("데이터셋이 복사된 위치: {}".format(train_dataset_fp))

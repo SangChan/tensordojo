43db4d16702e419938bd8a4b339beaf8c64cf2af
Sangchan <sangchan82@gmail.com>
2020-04-10 17:43:47 +0900

    add new

--- 20200404.py
+++ 20200404.py
@@ -13,3 +13,25 @@ train_dataset_fp = tf.keras.utils.get_file(fname=os.path.basename(train_dataset_
                                            origin=train_dataset_url)
 
 print("데이터셋이 복사된 위치: {}".format(train_dataset_fp))
+
+!head -n5 {train_dataset_fp}
+
+# CSV 파일안에서 컬럼의 순서
+column_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
+
+feature_names = column_names[:-1]
+label_name = column_names[-1]
+
+print("특성: {}".format(feature_names))
+print("레이블: {}".format(label_name))
+
+class_names = ['Iris setosa', 'Iris versicolor', 'Iris virginica']
+
+batch_size = 32
+
+train_dataset = tf.data.experimental.make_csv_dataset(
+    train_dataset_fp,
+    batch_size,
+    column_names=column_names,
+    label_name=label_name,
+    num_epochs=1)

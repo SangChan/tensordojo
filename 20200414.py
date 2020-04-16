8022dd77e5196999ad2896c51f75b63e9837d38b
SangChan Lee <sangchan82@gmail.com>
2020-04-17 07:58:26 +0900

    add new

--- 20200414.py
+++ 20200414.py
@@ -1,1 +1,20 @@
-# 20200414
+# 20200414 : Better performance with tf.function
+
+import tensorflow as tf
+
+import traceback
+import contextlib
+
+# Some helper code to demonstrate the kinds of errors you might encounter.
+@contextlib.contextmanager
+def assert_raises(error_class):
+  try:
+    yield
+  except error_class as e:
+    print('Caught expected exception \n  {}:'.format(error_class))
+    traceback.print_exc(limit=2)
+  except Exception as e:
+    raise e
+  else:
+    raise Exception('Expected {} to be raised but no error was raised!'.format(
+        error_class))

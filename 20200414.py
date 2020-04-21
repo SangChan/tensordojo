991859c332785393c9de8722f25f3ba70daa7c0a
Sangchan <sangchan82@gmail.com>
2020-04-21 18:11:46 +0900

    add new

--- 20200414.py
+++ 20200414.py
@@ -30,3 +30,42 @@ with tf.GradientTape() as tape:
   result = add(v, 1.0)
 tape.gradient(result, v)
 print(result)
+
+@tf.function
+def dense_layer(x, w, b):
+  return add(tf.matmul(x, w), b)
+
+dense_layer(tf.ones([3, 2]), tf.ones([2, 2]), tf.ones([2]))
+
+# Functions are polymorphic
+
+@tf.function
+def double(a):
+  print("Tracing with", a)
+  return a + a
+
+print(double(tf.constant(1)))
+print()
+print(double(tf.constant(1.1)))
+print()
+print(double(tf.constant("a")))
+print()
+
+print("Obtaining concrete trace")
+double_strings = double.get_concrete_function(tf.TensorSpec(shape=None, dtype=tf.string))
+print("Executing traced function")
+print(double_strings(tf.constant("a")))
+print(double_strings(a=tf.constant("b")))
+print("Using a concrete trace with incompatible types will throw an error")
+with assert_raises(tf.errors.InvalidArgumentError):
+  double_strings(tf.constant(1))
+
+@tf.function(input_signature=(tf.TensorSpec(shape=[None], dtype=tf.int32),))
+def next_collatz(x):
+  print("Tracing with", x)
+  return tf.where(x % 2 == 0, x // 2, 3 * x + 1)
+
+print(next_collatz(tf.constant([1, 2])))
+# We specified a 1-D tensor in the input signature, so this should fail.
+with assert_raises(ValueError):
+  next_collatz(tf.constant([[1, 2], [3, 4]]))

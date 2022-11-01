dd57da0d9aaa5f9da2c32921842b085164b53ed7
Sangchan <sangchan82@gmail.com>
2022-11-01 21:25:44 +0900

    add new

--- ch01.py
+++ ch01.py
@@ -8,3 +8,9 @@ a = tf.constant(10)
 b = tf.constant (32)
 c = tf.add(a,b).numpy()
 print(c)
+
+@tf.function
+def adder(a, b):
+    return a+b
+x = tf.Variable(tf.ones(shape=[None, 3]), dtype=tf.float32)
+print(x)

56412032e71f9ea24b8d29e5cdb9d0bb9a3b6c4a
Sangchan <sangchan82@gmail.com>
2022-11-03 19:54:47 +0900

    add new'

--- ch03.py
+++ ch03.py
@@ -48,3 +48,26 @@ for i in range(1000):
     if i % 100 == 99:
         print(i, error, output)
 
+import numpy as np
+
+x = np.array([[1,1], [1,0], [0,1], [0,0]])
+y = np.array([[1], [0], [0], [0]])
+w = tf.random.normal([2],0,1)
+b = tf.random.normal([1],0,1)
+b_x = 1
+
+for i in range(2000):
+    error_sum = 0
+    for j in range(4):
+        output = sigmoid(np.sum(x[j]*w)+b_x*b)
+        error = y[j][0] - output
+        w = w + x[j] * 0.1 * error
+        b = b + b_x * 0.1 * error
+        error_sum += error
+    
+    if i % 200 == 199:
+        print(i, error_sum)
+
+
+for i in range(4):
+    print("X:", x[i], "Y:", y[i], "Output:", sigmoid(np.sum(x[i]*w)+b))

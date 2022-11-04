d1773e8e4f72f4674a4b44a5a8e01818ef98bed5
Sangchan <sangchan82@gmail.com>
2022-11-04 18:58:18 +0900

    add new

--- ch03.py
+++ ch03.py
@@ -71,3 +71,39 @@ for i in range(2000):
 
 for i in range(4):
     print("X:", x[i], "Y:", y[i], "Output:", sigmoid(np.sum(x[i]*w)+b))
+
+x = np.array([[1,1], [1,0], [0,1], [0,0]])
+y = np.array([[0], [1], [1], [0]])
+w = tf.random.normal([2],0,1)
+b = tf.random.normal([1],0,1)
+b_x = 1
+
+for i in range(2000):
+    error_sum = 0
+    for j in range(4):
+        output = sigmoid(np.sum(x[j]*w)+b_x*b)
+        error = y[j][0] - output
+        w = w + x[j] * 0.1 * error
+        b = b + b_x * 0.1 * error
+        error_sum += error
+    
+    if i % 200 == 199:
+        print(i, error_sum)
+
+
+for i in range(4):
+    print("X:", x[i], "Y:", y[i], "Output:", sigmoid(np.sum(x[i]*w)+b))
+
+x = np.array([[1,1],[1,0],[0,1],[0,0]])
+y = np.array([[0],[1],[1],[0]])
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(units=2, activation='sigmoid', input_shape=(2,)),
+    tf.keras.layers.Dense(units=1, activation='sigmoid')
+])
+
+model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.1), loss='mse')
+model.summary()
+
+history = model.fit(x,y, epochs=2000, batch_size=1)
+model.predict(x)

fc91f2d4dcc18e6619e6c50c9e0349af8f7a207a
Sangchan <sangchan82@gmail.com>
2022-11-12 18:48:56 +0900

    add new

--- ch04_4.py
+++ ch04_4.py
@@ -6,19 +6,26 @@ import random
 X = [0.3, -0.78, 1.26, 0.03, 1.11, 0.24, -0.24, -0.47, -0.77, -0.37, -0.85, -0.41, -0.27, 0.02, -0.76, 2.66]
 Y = [12.27, 14.44, 11.87, 18.75, 17.52, 16.37, 19.78, 19.51, 12.65, 14.74, 10.72, 21.94, 12.83, 15.51, 17.14, 14.42]
 
-x_bar = sum(X) / len(X)
-y_bar = sum(Y) / len(Y)
+a = tf.Variable(random.random())
+b = tf.Variable(random.random())
 
-a = sum([(y- y_bar) * (x - x_bar) for y,x in list(zip(Y,X))])
-a /= sum([(x- x_bar) ** 2 for x in X])
-b = y_bar - a * x_bar
-print("a:", a, "b:", b)
+def compute_loss():
+    y_pred = a * X + b
+    loss = tf.reduce_mean((Y - y_pred) ** 2)
+    return loss
+
+optimizer = tf.optimizers.Adam(learning_rate=0.07)
+for i in range(1000):
+    optimizer.minimize(compute_loss, var_list = [a,b])
+
+    if i % 100 == 99:
+        print(i, 'a:', a.numpy(), 'b:', b.numpy(), 'loss:', compute_loss().numpy())
+    
 
 line_x = np.arange(min(X), max(X), 0.01)
 line_y = a * line_x + b
 
 plt.plot(line_x, line_y,'r-')
-
 plt.plot(X,Y,"bo")
 plt.xlabel("Popluation Growth Rate (%)")
 plt.ylabel("Elderly Population Rate(%)")

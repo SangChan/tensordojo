3215e35309c96757480144d95e9b8eab474336ef
Sangchan <sangchan82@gmail.com>
2022-11-19 11:58:06 +0900

    add new

--- ch04_11.py
+++ ch04_11.py
@@ -3,3 +3,20 @@ from tensorflow.keras.datasets import boston_housing
 print(len(train_X), len(test_X))
 print(train_X[0])
 print(train_Y[0])
+
+x_mean = train_X.mean()
+x_std = train_X.std()
+train_X -= x_mean
+train_X /= x_std
+test_X -= x_mean
+test_X /= x_std
+
+y_mean = train_Y.mean()
+y_std = train_Y.std()
+train_Y -= y_mean
+train_Y /= y_std
+test_Y -= y_mean
+test_Y /= y_std
+
+print(train_X[0])
+print(train_Y[0])

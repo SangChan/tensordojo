d47bc4fffdae54271de9cb8d05eec5740dddbefd
Sangchan <sangchan82@gmail.com>
2022-11-30 18:26:35 +0900

    add new

--- ch05.py
+++ ch05.py
@@ -1,1 +1,5 @@
-import tensorflow as tf
+import pandas as pd
+red = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv', sep=';')
+white = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv', sep=';')
+print(red.head())
+print(white.head())

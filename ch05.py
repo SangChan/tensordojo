16c9047fec1dd79f66793304b795acd37a0fd551
Sangchan <sangchan82@gmail.com>
2022-12-01 19:58:59 +0900

    add new

--- ch05.py
+++ ch05.py
@@ -3,3 +3,17 @@ red = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/wine
 white = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv', sep=';')
 print(red.head())
 print(white.head())
+
+red['type'] = 0
+white['type'] = 1
+print(red.head(2))
+print(white.head(2))
+wine = pd.concat([red, white])
+print(wine.describe())
+
+import matplotlib.pyplot as plt
+plt.hist(wine['type'])
+plt.xticks([0,1])
+plt.show()
+
+print(wine['type'].value_counts())

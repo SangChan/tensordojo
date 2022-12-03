1f17b5ff3cac7847a9a2434acc739fb433533b66
Sangchan <sangchan82@gmail.com>
2022-12-03 17:02:46 +0900

    add new

--- ch05.py
+++ ch05.py
@@ -32,3 +32,41 @@ print(wine_np[:5])
 import tensorflow as tf
 train_idx = int(len(wine_np) * 0.8)
 train_X, train_Y = wine_np[:train_idx, :-1], wine_np[:train_idx, -1]
+test_X, test_Y = wine_np[train_idx:, :-1], wine_np[train_idx:, -1]
+print(train_X[0])
+print(train_Y[0])
+print(test_X[0])
+print(test_Y[0])
+train_Y = tf.keras.utils.to_categorical(train_Y, num_classes=2)
+test_Y = tf.keras.utils.to_categorical(test_Y, num_classes=2)
+print(train_X[0])
+print(test_Y[0])
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(units=48, activation='relu', input_shape=(12,)),
+    tf.keras.layers.Dense(units=24, activation='relu'),
+    tf.keras.layers.Dense(units=12, activation='relu'),
+    tf.keras.layers.Dense(units=2, activation='softmax'),
+])
+
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.07), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+
+history = model.fit(train_X, train_Y, epochs=25, batch_size=32, validation_split=0.25)
+
+plt.figure(figsize=(12,4))
+plt.subplot(1,2,1)
+plt.plot(history.history['loss'], 'b-', label='loss')
+plt.plot(history.history['val_loss'], 'r--', label='val_loss')
+plt.xlabel('Epoch')
+plt.legend()
+
+plt.subplot(1,2,2)
+plt.plot(history.history['accuracy'], 'g-', label='accuracy')
+plt.plot(history.history['val_accuracy'], 'k--', label='val_accuracy')
+plt.xlabel('Epoch')
+plt.ylim(0.7, 1)
+plt.legend()
+plt.show()
+
+model.evaluate(test_X, test_Y)

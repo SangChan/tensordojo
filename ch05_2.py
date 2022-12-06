2faf0df22b39bf760503c6ba2dec69d36bd7307c
Sangchan <sangchan82@gmail.com>
2022-12-06 19:55:00 +0900

    add new

--- ch05_2.py
+++ ch05_2.py
@@ -4,3 +4,14 @@ fashion_mnist = tf.keras.datasets.fashion_mnist
 (train_X, train_Y), (test_X, test_Y) = fashion_mnist.load_data()
 
 print(len(train_X), len(test_X))
+
+import matplotlib.pyplot as plt
+plt.imshow(train_X[0], cmap='gray')
+plt.colorbar()
+plt.show()
+
+print(train_Y[0])
+
+train_X = train_X / 255.0
+test_X = test_X / 255.0
+print(train_X[0])

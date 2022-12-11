57dc412d6dd13e3f467aaf68226abd1968a5cd3e
Sangchan <sangchan82@gmail.com>
2022-12-11 18:53:24 +0900

    add new

--- ch06.py
+++ ch06.py
@@ -12,3 +12,13 @@ train_X = train_X.reshape(-1,28,28,1)
 test_X = test_X.reshape(-1,28,28,1)
 
 print(train_X.shape, test_X.shape)
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10,10))
+for c in range(16):
+    plt.subplot(4,4,c+1)
+    plt.imshow(train_X[c].reshape(28,28), cmap='gray')
+
+plt.show()
+print(train_Y[:16])

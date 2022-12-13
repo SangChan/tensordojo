b1e9b238a2b760c4536b0dfd6841bd1e0cb98c01
Sangchan <sangchan82@gmail.com>
2022-12-13 18:28:20 +0900

    add new

--- ch06.py
+++ ch06.py
@@ -34,3 +34,25 @@ model = tf.keras.Sequential([
 
 model.compile(optimizer=tf.keras.optimizers.Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.summary()
+
+history = model.fit(train_X, train_Y, epochs=25, validation_split=0.25)
+
+import matplotlib.pyplot as plt
+plt.figure(figsize=(12,4))
+
+plt.subplot(1,2,1)
+plt.plot(history.history['loss'], 'b-', label='loss')
+plt.plot(history.history['val_loss'], 'r--', label='val_loss')
+plt.xlabel('Epoch')
+plt.legend()
+
+plt.subplot(1,2,2)
+plt.plot(history.history['accuracy'], 'g-', label='accuracy')
+plt.plot(history.history['val_accuracy'], 'k--', label='val_accuracy')
+plt.xlabel('Epoch')
+plt.ylim(0.7, 1)
+plt.legend()
+
+plt.show()
+
+model.evaluate(test_X, test_Y, verbose=0)

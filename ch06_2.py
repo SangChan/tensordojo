5e4caa606ee4d32d308c5054083597acf89cd33c
Sangchan <sangchan82@gmail.com>
2022-12-20 17:58:12 +0900

    add new

--- ch06_2.py
+++ ch06_2.py
@@ -21,3 +21,44 @@ for c in range(16):
 
 plt.show()
 print(train_Y[:16])
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(input_shape=(28,28,1), kernel_size=(3,3), filters=32, padding='same', activation='relu'),
+    tf.keras.layers.Conv2D(kernel_size=(3,3), filters=64, padding='same', activation='relu'),
+    tf.keras.layers.MaxPool2D(strides=(2,2)),
+    tf.keras.layers.Dropout(rate=0.5),
+    tf.keras.layers.Conv2D(kernel_size=(3,3), filters=128, padding='same', activation='relu'),
+    tf.keras.layers.Conv2D(kernel_size=(3,3), filters=256, padding='valid', activation='relu'),
+    tf.keras.layers.MaxPool2D(strides=(2,2)),
+    tf.keras.layers.Dropout(rate=0.5),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(units=512, activation='relu'),
+    tf.keras.layers.Dropout(rate=0.5),
+    tf.keras.layers.Dense(units=256, activation='relu'),
+    tf.keras.layers.Dropout(rate=0.5),
+    tf.keras.layers.Dense(units=10, activation='softmax')
+])
+
+model.compile(optimizer=tf.keras.optimizers.Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+
+history = model.fit(train_X, train_Y, epochs=25, validation_split=0.25)
+
+plt.figure(figsize=(12,4))
+
+plt.subplot(1,2,1)
+plt.plot(history.history['loss'], 'b-', label='loss')
+plt.plot(history.history['val_loss'], 'r--', label='val_loss')
+plt.xlabel('Epoch')
+plt.legend()
+
+plt.subplot(1,2,2)
+plt.plot(history.history['accuracy'], 'g-', label='accuracy')
+plt.plot(history.history['val_accuracy'], 'k--', label='val_accuracy')
+plt.xlabel('Epoch')
+plt.ylim(0.7, 1)
+plt.legend()
+
+plt.show()
+
+model.evaluate(test_X, test_Y, verbose=0)

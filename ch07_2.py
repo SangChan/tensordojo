2b39866992a35bf391aad12c33669d418250fdc0
Sangchan <sangchan82@gmail.com>
2022-12-27 17:29:45 +0900

    add new

--- ch07_2.py
+++ ch07_2.py
@@ -1,5 +1,15 @@
 import numpy as np
 import tensorflow as tf
 
+X = []
+Y = []
+
 for i in range(3000):
     lst = np.random.rand(100)
+    idx = np.random.choice(100, 2, replace=False)
+    zeros = np.zeros(100)
+    zeros[idx] = 1
+    X.append(np.array(list(zip(zeros, lst))))
+    Y.append(np.prod(lst[idx]))
+
+print(X[0], Y[0])

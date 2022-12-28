448e62c0299ab1b100e6cd328beac6465f0389dd
Sangchan <sangchan82@gmail.com>
2022-12-28 22:49:29 +0900

    add new

--- ch07_2.py
+++ ch07_2.py
@@ -13,3 +13,11 @@ for i in range(3000):
     Y.append(np.prod(lst[idx]))
 
 print(X[0], Y[0])
+
+model = tf.keras.Sequential([
+   tf.keras.layers.SimpleRNN(units=30, return_sequences=True, input_shape=[100,2]),
+   tf.keras.layers.SimpleRNN(units=30),
+   tf.keras.layers.Dense(1) 
+])
+model.compile(optimizer='adam', loss='mse')
+model.summary()

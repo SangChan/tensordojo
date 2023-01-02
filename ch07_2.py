73f07a7742629042c224060f1562c9caa9c14210
Sangchan <sangchan82@gmail.com>
2023-01-02 18:48:10 +0900

    add new

--- ch07_2.py
+++ ch07_2.py
@@ -47,3 +47,35 @@ for i in range(len(prediction)):
         fail += 1
 
 print('correctness:', (440-fail) / 440 *100, '%')
+
+model = tf.keras.Sequential([
+    tf.keras.layers.LSTM(units=30, return_sequences=True, input_shape=[100,2]),
+    tf.keras.layers.LSTM(units=30),
+    tf.keras.layers.Dense(1)
+])
+
+model.compile(optimizer='adam', loss='mse')
+model.summary()
+
+X = np.array(X)
+Y = np.array(Y)
+history = model.fix(X[:2560], Y[:2560], epochs= 100, validation_split=0.2)
+
+plt.plot(history.history['loss'], 'b-', label='loss')
+plt.plot(history.history['val_loss'], 'r--', label='val_loss')
+plt.xlabel('Epoch')
+plt.legend()
+plt.show()
+
+model.evaluate(X[2560:], Y[2560:])
+prediction = model.predict(X[2560:2560+5])
+for i in range(5):
+    print(Y[2560+i], '\t', prediction[i][0], '\tdiff:', abs(prediction[i][0] - Y[2560+i]))
+
+prediction = model.predict(X[2560:])
+cnt = 0
+for i in range(len(prediction)):
+    if abs(prediction[i][0] - Y[2560+i]) > 0.04:
+        cnt += 1
+
+print('correctness:', (440-cnt)/440 * 100, '%')

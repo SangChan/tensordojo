95fc6ebd3f2ddc564af6a0d8af2380312180f6fd
Sangchan <sangchan82@gmail.com>
2023-01-05 19:19:30 +0900

    add new

--- ch07_3.py
+++ ch07_3.py
@@ -2,3 +2,12 @@ import tensorflow as tf
 
 path_to_train_file = tf.keras.utils.get_file('train.txt', 'https://raw.githubusercontent.com/e9t/nsmc/master/ratings_train.txt')
 path_to_test_file = tf.keras.utils.get_file('test.txt', 'https://raw.githubusercontent.com/e9t/nsmc/master/ratings_test.txt')
+
+train_text = open(path_to_train_file, 'rb').read().decode(encoding='utf-8')
+test_text = open(path_to_test_file, 'rb').read().decode(encoding='utf-8')
+
+print('Length of trained text: {} characters'.format(len(train_text)))
+print('Length of test puporse text: {} characters'.format(len(test_text)))
+print()
+
+print(train_text[:300])

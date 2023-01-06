a85151d4fc8f8ffbbbd59c34e06693809a54b2c8
Sangchan <sangchan82@gmail.com>
2023-01-06 18:13:35 +0900

    add new

--- ch07_3.py
+++ ch07_3.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 path_to_train_file = tf.keras.utils.get_file('train.txt', 'https://raw.githubusercontent.com/e9t/nsmc/master/ratings_train.txt')
 path_to_test_file = tf.keras.utils.get_file('test.txt', 'https://raw.githubusercontent.com/e9t/nsmc/master/ratings_test.txt')
@@ -11,3 +12,8 @@ print('Length of test puporse text: {} characters'.format(len(test_text)))
 print()
 
 print(train_text[:300])
+
+train_Y = np.array([[int(row.split('\t')[2])] for row in train_text.split('\n')[1:] if row.count('\t') > 0])
+test_Y = np.array([[int(row.split('\t')[2])] for row in test_text.split('\n')[1:] if row.count('\t') > 0])
+print(train_Y.shape, test_Y.shape)
+print(train_Y[:5])

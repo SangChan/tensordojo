509949407471cb127a20c989bdea61325be915f1
Sangchan <sangchan82@gmail.com>
2023-01-09 18:01:46 +0900

    add third part from chapter7

--- ch07_3.py
+++ ch07_3.py
@@ -17,3 +17,121 @@ train_Y = np.array([[int(row.split('\t')[2])] for row in train_text.split('\n')[
 test_Y = np.array([[int(row.split('\t')[2])] for row in test_text.split('\n')[1:] if row.count('\t') > 0])
 print(train_Y.shape, test_Y.shape)
 print(train_Y[:5])
+
+import re
+# From https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
+def clean_str(string):    
+    string = re.sub(r"[^가-힣A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"n\'t", " n\'t", string)
+    string = re.sub(r"\'re", " \'re", string)
+    string = re.sub(r"\'d", " \'d", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", " \( ", string)
+    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\?", " \? ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    string = re.sub(r"\'{2,}", "\'", string)
+    string = re.sub(r"\'", "", string)
+
+    return string.lower()
+
+
+train_text_X = [row.split('\t')[1] for row in train_text.split('\n')[1:] if row.count('\t') > 0]
+train_text_X = [clean_str(sentence) for sentence in train_text_X]
+# 문장을 띄어쓰기 단위로 단어 분리
+sentences = [sentence.split(' ') for sentence in train_text_X]
+for i in range(5):
+    print(sentences[i])
+
+import matplotlib.pyplot as plt
+sentence_len = [len(sentence) for sentence in sentences]
+sentence_len.sort()
+plt.plot(sentence_len)
+plt.show()
+
+print(sum([int(l<=25) for l in sentence_len]))
+
+sentences_new = []
+for sentence in sentences:
+    sentences_new.append([word[:5] for word in sentence][:25])
+sentences = sentences_new
+for i in range(5):
+    print(sentences[i])
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+tokenizer = Tokenizer(num_words=20000)
+tokenizer.fit_on_texts(sentences)
+train_X = tokenizer.texts_to_sequences(sentences)
+train_X = pad_sequences(train_X, padding='post')
+
+print(train_X[:5])
+
+print(tokenizer.index_word[19999])
+print(tokenizer.index_word[20000])
+temp = tokenizer.texts_to_sequences(['###', '경우는', '잊혀질', '연기가'])
+print(temp)
+temp = pad_sequences(temp, padding='post')
+print(temp)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(20000, 300, input_length=25),
+    tf.keras.layers.LSTM(units=50),
+    tf.keras.layers.Dense(2, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+
+history = model.fit(train_X, train_Y, epochs=5, batch_size=128, validation_split=0.2)
+
+import matplotlib.pyplot as plt
+plt.figure(figsize=(12, 4))
+
+plt.subplot(1, 2, 1)
+plt.plot(history.history['loss'], 'b-', label='loss')
+plt.plot(history.history['val_loss'], 'r--', label='val_loss')
+plt.xlabel('Epoch')
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(history.history['accuracy'], 'g-', label='accuracy')
+plt.plot(history.history['val_accuracy'], 'k--', label='val_accuracy')
+plt.xlabel('Epoch')
+plt.ylim(0.7, 1)
+plt.legend()
+
+plt.show()
+     
+test_text_X = [row.split('\t')[1] for row in test_text.split('\n')[1:] if row.count('\t') > 0]
+test_text_X = [clean_str(sentence) for sentence in test_text_X]
+sentences = [sentence.split(' ') for sentence in test_text_X]
+sentences_new = []
+for sentence in sentences:
+    sentences_new.append([word[:5] for word in sentence][:25])
+sentences = sentences_new
+
+test_X = tokenizer.texts_to_sequences(sentences)
+test_X = pad_sequences(test_X, padding='post')
+
+model.evaluate(test_X, test_Y, verbose=0)
+
+test_sentence = '재미있을 줄 알았는데 완전 실망했다. 너무 졸리고 돈이 아까웠다.'
+test_sentence = test_sentence.split(' ')
+test_sentences = []
+now_sentence = []
+for word in test_sentence:
+    now_sentence.append(word)
+    test_sentences.append(now_sentence[:])
+    
+test_X_1 = tokenizer.texts_to_sequences(test_sentences)
+test_X_1 = pad_sequences(test_X_1, padding='post', maxlen=25)
+prediction = model.predict(test_X_1)
+for idx, sentence in enumerate(test_sentences):
+    print(sentence)
+    print(prediction[idx])

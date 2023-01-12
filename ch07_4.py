c23f07d41d58c1abfe70eea2fe084bc59046452d
Sangchan <sangchan82@gmail.com>
2023-01-12 19:19:19 +0900

    add new

--- ch07_4.py
+++ ch07_4.py
@@ -1,3 +1,150 @@
 import tensorflow as tf
+import numpy as np
 
 path_to_file = tf.keras.utils.get_file('input.txt', 'http://bit.ly/2Mc3SOV')
+
+# 7.33 데이터 로드 및 확인
+# 데이터를 메모리에 불러옵니다. encoding 형식으로 utf-8 을 지정해야합니다.
+train_text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
+
+# 텍스트가 총 몇 자인지 확인합니다.
+print('Length of text: {} characters'.format(len(train_text)))
+print()
+
+# 처음 100 자를 확인해봅니다.
+print(train_text[:100])
+
+# 7.34 훈련 데이터 입력 정제
+import re
+# From https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
+def clean_str(string):    
+    string = re.sub(r"[^가-힣A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", "", string)
+    string = re.sub(r"\)", "", string)
+    string = re.sub(r"\?", " \? ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    string = re.sub(r"\'{2,}", "\'", string)
+    string = re.sub(r"\'", "", string)
+
+    return string
+
+
+train_text = train_text.split('\n')
+train_text = [clean_str(sentence) for sentence in train_text]
+train_text_X = []
+for sentence in train_text:
+    train_text_X.extend(sentence.split(' '))
+    train_text_X.append('\n')
+    
+train_text_X = [word for word in train_text_X if word != '']
+
+print(train_text_X[:20])
+
+# 7.35 단어 토큰화
+# 단어의 set을 만듭니다.
+vocab = sorted(set(train_text_X))
+vocab.append('UNK')
+print ('{} unique words'.format(len(vocab)))
+
+# vocab list를 숫자로 맵핑하고, 반대도 실행합니다.
+word2idx = {u:i for i, u in enumerate(vocab)}
+idx2word = np.array(vocab)
+
+text_as_int = np.array([word2idx[c] for c in train_text_X])
+
+# word2idx 의 일부를 알아보기 쉽게 print 해봅니다.
+print('{')
+for word,_ in zip(word2idx, range(10)):
+    print('  {:4s}: {:3d},'.format(repr(word), word2idx[word]))
+print('  ...\n}')
+
+print('index of UNK: {}'.format(word2idx['UNK']))
+
+# 7.36 토큰 데이터 확인
+print(train_text_X[:20])
+print(text_as_int[:20])
+
+# 7.37 기본 데이터셋 만들기
+seq_length = 25
+examples_per_epoch = len(text_as_int) // seq_length
+sentence_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+
+sentence_dataset = sentence_dataset.batch(seq_length+1, drop_remainder=True)
+for item in sentence_dataset.take(1):
+    print(idx2word[item.numpy()])
+    print(item.numpy())
+
+# 7.38 학습 데이터셋 만들기
+def split_input_target(chunk):
+    return [chunk[:-1], chunk[-1]]
+
+train_dataset = sentence_dataset.map(split_input_target)
+for x,y in train_dataset.take(1):
+    print(idx2word[x.numpy()])
+    print(x.numpy())
+    print(idx2word[y.numpy()])
+    print(y.numpy())
+
+# 7.39 데이터셋 shuffle, batch 설정
+BATCH_SIZE = 512
+steps_per_epoch = examples_per_epoch // BATCH_SIZE
+BUFFER_SIZE = 10000
+
+train_dataset = train_dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+
+# 7.40 단어 단위 생성 모델 정의
+total_words = len(vocab)
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(total_words, 100, input_length=seq_length),
+    tf.keras.layers.LSTM(units=100, return_sequences=True),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.LSTM(units=100),
+    tf.keras.layers.Dense(total_words, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+
+# 7.41 단어 단위 생성 모델 학습
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+def testmodel(epoch, logs):
+    if epoch % 5 != 0 and epoch != 49:
+        return
+    test_sentence = train_text[0]
+
+    next_words = 100
+    for _ in range(next_words):
+        test_text_X = test_sentence.split(' ')[-seq_length:]
+        test_text_X = np.array([word2idx[c] if c in word2idx else word2idx['UNK'] for c in test_text_X])
+        test_text_X = pad_sequences([test_text_X], maxlen=seq_length, padding='pre', value=word2idx['UNK'])
+
+        output_idx = model.predict_classes(test_text_X)
+        test_sentence += ' ' + idx2word[output_idx[0]]
+    
+    print()
+    print(test_sentence)
+    print()
+
+testmodelcb = tf.keras.callbacks.LambdaCallback(on_epoch_end=testmodel)
+
+history = model.fit(train_dataset.repeat(), epochs=50, steps_per_epoch=steps_per_epoch, callbacks=[testmodelcb], verbose=2)
+
+# 7.42 임의의 문장을 사용한 생성 결과 확인
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+test_sentence = '동헌에 나가 공무를 본 후 활 십오 순을 쏘았다'
+
+next_words = 100
+for _ in range(next_words):
+    test_text_X = test_sentence.split(' ')[-seq_length:]
+    test_text_X = np.array([word2idx[c] if c in word2idx else word2idx['UNK'] for c in test_text_X])
+    test_text_X = pad_sequences([test_text_X], maxlen=seq_length, padding='pre', value=word2idx['UNK'])
+    
+    output_idx = model.predict_classes(test_text_X)
+    test_sentence += ' ' + idx2word[output_idx[0]]
+
+print(test_sentence)
+     

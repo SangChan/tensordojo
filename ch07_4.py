480bcff87fc5d290f7d26ebbe2458654b76b2006
Sangchan <sangchan82@gmail.com>
2023-01-13 16:26:36 +0900

    complete chapter7

--- ch07_4.py
+++ ch07_4.py
@@ -147,4 +147,124 @@ for _ in range(next_words):
     test_sentence += ' ' + idx2word[output_idx[0]]
 
 print(test_sentence)
-     
+
+import jamotools
+
+train_text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
+s = train_text[:100]
+print(s)
+
+# 한글 텍스트를 자모 단위로 분리해줍니다. 한자 등에는 영향이 없습니다.
+s_split = jamotools.split_syllables(s)
+print(s_split)
+
+# 7.45 자모 결합 테스트
+s2 = jamotools.join_jamos(s_split)
+print(s2)
+print(s == s2)
+
+# 7.46 자모 토큰화
+# 텍스트를 자모 단위로 나눕니다. 데이터가 크기 때문에 약간 시간이 걸립니다.
+train_text_X = jamotools.split_syllables(train_text)
+vocab = sorted(set(train_text_X))
+vocab.append('UNK')
+print ('{} unique characters'.format(len(vocab)))
+
+# vocab list를 숫자로 맵핑하고, 반대도 실행합니다.
+char2idx = {u:i for i, u in enumerate(vocab)}
+idx2char = np.array(vocab)
+
+text_as_int = np.array([char2idx[c] for c in train_text_X])
+
+# word2idx 의 일부를 알아보기 쉽게 print 해봅니다.
+print('{')
+for char,_ in zip(char2idx, range(10)):
+    print('  {:4s}: {:3d},'.format(repr(char), char2idx[char]))
+print('  ...\n}')
+
+print('index of UNK: {}'.format(char2idx['UNK']))
+
+# 7.47 토큰 데이터 확인
+print(train_text_X[:20])
+print(text_as_int[:20])
+
+# 7.48 학습 데이터세트 생성
+seq_length = 80
+examples_per_epoch = len(text_as_int) // seq_length
+char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+
+char_dataset = char_dataset.batch(seq_length+1, drop_remainder=True)
+for item in char_dataset.take(1):
+    print(idx2char[item.numpy()])
+    print(item.numpy())
+    
+def split_input_target(chunk):
+    return [chunk[:-1], chunk[-1]]
+
+train_dataset = char_dataset.map(split_input_target)
+for x,y in train_dataset.take(1):
+    print(idx2char[x.numpy()])
+    print(x.numpy())
+    print(idx2char[y.numpy()])
+    print(y.numpy())
+    
+BATCH_SIZE = 256
+steps_per_epoch = examples_per_epoch // BATCH_SIZE
+BUFFER_SIZE = 10000
+
+train_dataset = train_dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+
+# 7.49 자소 단위 생성 모델 정의
+total_chars = len(vocab)
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(total_chars, 100, input_length=seq_length),
+    tf.keras.layers.LSTM(units=400),
+    tf.keras.layers.Dense(total_chars, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+
+# 7.50 자소 단위 생성 모델 학습
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+def testmodel(epoch, logs):
+    if epoch % 5 != 0 and epoch != 99:
+        return
+    
+    test_sentence = train_text[:48]
+    test_sentence = jamotools.split_syllables(test_sentence)
+
+    next_chars = 300
+    for _ in range(next_chars):
+        test_text_X = test_sentence[-seq_length:]
+        test_text_X = np.array([char2idx[c] if c in char2idx else char2idx['UNK'] for c in test_text_X])
+        test_text_X = pad_sequences([test_text_X], maxlen=seq_length, padding='pre', value=char2idx['UNK'])
+
+        output_idx = model.predict_classes(test_text_X)
+        test_sentence += idx2char[output_idx[0]]
+    
+    print()
+    print(jamotools.join_jamos(test_sentence))
+    print()
+
+testmodelcb = tf.keras.callbacks.LambdaCallback(on_epoch_end=testmodel)
+
+history = model.fit(train_dataset.repeat(), epochs=100, steps_per_epoch=steps_per_epoch, callbacks=[testmodelcb], verbose=2)
+
+# 7.51 임의의 문장을 사용한 생성 결과 확인
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+test_sentence = '동헌에 나가 공무를 본 후 활 십오 순을 쏘았다'
+test_sentence = jamotools.split_syllables(test_sentence)
+
+next_chars = 300
+for _ in range(next_chars):
+    test_text_X = test_sentence[-seq_length:]
+    test_text_X = np.array([char2idx[c] if c in char2idx else char2idx['UNK'] for c in test_text_X])
+    test_text_X = pad_sequences([test_text_X], maxlen=seq_length, padding='pre', value=char2idx['UNK'])
+    
+    output_idx = model.predict_classes(test_text_X)
+    test_sentence += idx2char[output_idx[0]]
+    
+
+print(jamotools.join_jamos(test_sentence))

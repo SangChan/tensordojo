1f8046ba3c10fa179fbeb49430267daf847d6600
Sangchan <sangchan82@gmail.com>
2023-01-31 17:12:36 +0900

    so hard to fit

--- ch08_2.py
+++ ch08_2.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 # 8.8 Stanford Dog Dataset을 Kaggle에서 불러오기
 
 # 2020.02.01 현재 kaggle의 Stanford Dog Dataset 파일 구조가 변경되었습니다. 
@@ -35,4 +36,69 @@ for c in range(9):
     plt.imshow(plt.imread('/Users/sangchan.lee/workspace/content/train/' + image_id + '.jpg'))
     plt.title(str(c) + ', ' + label_text.loc[c, 'breed'])
     plt.axis('off')
+plt.show()
+
+# 8.14 tf.keras에서 MobileNet V2 불러오기
+from tensorflow.keras.applications import MobileNetV2
+mobilev2 = MobileNetV2()
+
+# 8.15 MobileNet V2의 가중치 초기화
+for layer in mobilev2.layers[:-1]:
+    layer.trainable = True
+    
+for layer in mobilev2.layers[:-1]: 
+    if 'kernel' in layer.__dict__:
+        kernel_shape = np.array(layer.get_weights()).shape
+        # weight를 평균이 0, 표준편차가 1인 random 변수로 초기화
+        layer.set_weights(tf.random.normal(kernel_shape, 0, 1))
+
+# 8.16 train 데이터를 메모리에 로드
+import cv2
+
+train_X = []
+for i in range(len(label_text)):
+    img = cv2.imread('/Users/sangchan.lee/workspace/content/train/' + label_text['id'][i] + '.jpg')
+    img = cv2.resize(img, dsize=(224, 224))
+    img = img / 255.0
+    train_X.append(img)
+train_X = np.array(train_X)
+print(train_X.shape)
+print(train_X.size * train_X.itemsize, ' bytes')
+
+# 8.17 train 라벨 데이터를 메모리에 로드
+unique_Y = label_text['breed'].unique().tolist()
+train_Y = [unique_Y.index(breed) for breed in label_text['breed']]
+train_Y = np.array(train_Y)
+
+print(train_Y[:10])
+print(train_Y[-10:])
+
+# 8.18 Dogs Dataset 학습을 위한 모델 정의
+x = mobilev2.layers[-2].output
+predictions = tf.keras.layers.Dense(120, activation='softmax')(x)
+model = tf.keras.Model(inputs=mobilev2.input, outputs=predictions)
+
+model.compile(optimizer='sgd', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+
+# 8.19 랜덤한 가중치를 가진 상태에서의 학습
+history = model.fit(train_X, train_Y, epochs=10, validation_split=0.25, batch_size=32)
+
+# 8.20 학습 결과 확인
+import matplotlib.pyplot as plt
+plt.figure(figsize=(12, 4))
+
+plt.subplot(1, 2, 1)
+plt.plot(history.history['loss'], 'b-', label='loss')
+plt.plot(history.history['val_loss'], 'r--', label='val_loss')
+plt.xlabel('Epoch')
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(history.history['accuracy'], 'g-', label='accuracy')
+plt.plot(history.history['val_accuracy'], 'k--', label='val_accuracy')
+plt.xlabel('Epoch')
+plt.ylim(0, 0.1)
+plt.legend()
+
 plt.show()

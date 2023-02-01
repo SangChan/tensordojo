03eadfa9b13c5fe246412d10d6d47294a7a77ad0
Sangchan <sangchan82@gmail.com>
2023-02-01 18:01:57 +0900

    add new

--- ch08_2.py
+++ ch08_2.py
@@ -102,3 +102,133 @@ plt.ylim(0, 0.1)
 plt.legend()
 
 plt.show()
+
+# 8.30 라벨 텍스트를 알파벳 순으로 정렬
+unique_sorted_Y = sorted(unique_Y)
+print(unique_sorted_Y)
+
+# 8.31 Inception V3 특징 추출기-Sequential 모델의 분류 라벨 확인
+import random
+plt.figure(figsize=(16,16))
+  
+for c in range(3):
+    image_path = random.choice(valid_generator.filepaths)
+    
+    # 이미지 표시
+    plt.subplot(3,2,c*2+1)
+    plt.imshow(plt.imread(image_path))
+    real_y = image_path.split('/')[3]
+    plt.title(real_y)
+    plt.axis('off')
+    idx = unique_sorted_Y.index(real_y)
+    
+    # 예측값 표시
+    plt.subplot(3,2,c*2+2)
+    img = cv2.imread(image_path)
+    img = cv2.resize(img, dsize=(299, 299))
+    img = img / 255.0
+    img = np.expand_dims(img, axis=0)
+    
+    # Inception V3를 이용한 특징 벡터 추출
+    feature_vector = feature_model.predict(img)
+    
+    # Sequential 모델을 이용한 예측
+    prediction = model.predict(feature_vector)[0]
+    
+    # 가장 높은 확률의 예측값 5개를 뽑음
+    top_5_predict = prediction.argsort()[::-1][:5]
+    labels = [unique_sorted_Y[index] for index in top_5_predict]
+    color = ['gray'] * 5
+    if idx in top_5_predict:
+        color[top_5_predict.tolist().index(idx)] = 'green'
+    color = color[::-1]
+    plt.barh(range(5), prediction[top_5_predict][::-1] * 100, color=color)
+    plt.yticks(range(5), labels[::-1])
+
+
+# 8.33 submission.csv 파일 내용 확인
+import pandas as pd
+submission = pd.read_csv('sample_submission.csv')
+print(submission.head())
+print()
+print(submission.info())
+
+# 8.34 ImageDataGenerator가 처리할 수 있는 하위 디렉토리 구조로 데이터 복사
+import os
+import shutil
+
+os.mkdir('/Users/sangchan.lee/workspace/content/test_sub/')
+os.mkdir('/Users/sangchan.lee/workspace/content/test_sub/unknown/')
+
+for i in range(len(submission)):
+    shutil.copy('/Users/sangchan.lee/workspace/content/test/' + submission.loc[i]['id'] + '.jpg', '/Users/sangchan.lee/workspace/content/test_sub/unknown/')
+     
+# 8.35 ImageDataGenerator를 이용한 test 데이터 불러오기
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+
+test_datagen=ImageDataGenerator(rescale=1./255.)
+test_generator=test_datagen.flow_from_directory(directory="/Users/sangchan.lee/workspace/content/test_sub/",batch_size=1,seed=42,shuffle=False,target_size=(299, 299))
+
+
+# 8.36 test 데이터를 특징 벡터로 변환
+test_features = []
+
+for idx in range(test_generator.n):
+    if idx % 100 == 0:
+        print(idx)
+        
+    x, _ = test_generator.next()
+    feature = feature_model.predict(x)
+    test_features.extend(feature)
+
+test_features = np.array(test_features)
+print(test_features.shape)
+
+# 8.37 특징 벡터로 test 데이터의 정답 예측
+test_Y = model.predict(test_features, verbose=1)
+
+# 8.38 Inception V3 특징 추출기-Sequential 모델의 test 데이터 분류 라벨 확인
+import random
+plt.figure(figsize=(16,16))
+  
+for c in range(3):
+    image_path = random.choice(test_generator.filepaths)
+    
+    # 이미지 표시
+    plt.subplot(3,2,c*2+1)
+    plt.imshow(plt.imread(image_path))
+    real_y = image_path.split('/')[3]
+    plt.title(real_y)
+    plt.axis('off')
+    
+    # 예측값 표시
+    plt.subplot(3,2,c*2+2)
+    img = cv2.imread(image_path)
+    img = cv2.resize(img, dsize=(299, 299))
+    img = img / 255.0
+    img = np.expand_dims(img, axis=0)
+    
+    # Inception V3를 이용한 특징 벡터 추출
+    feature_vector = feature_model.predict(img)
+    
+    # Sequential 모델을 이용한 예측
+    prediction = model.predict(feature_vector)[0]
+    
+    # 가장 높은 확률의 예측값 5개를 뽑음
+    top_5_predict = prediction.argsort()[::-1][:5]
+    labels = [unique_sorted_Y[index] for index in top_5_predict]
+    color = ['gray'] * 5
+    plt.barh(range(5), prediction[top_5_predict][::-1] * 100, color=color)
+    plt.yticks(range(5), labels[::-1])
+
+# 8.39 submission 데이터프레임에 예측값 저장
+for i in range(len(test_Y)):
+    for j in range(len(test_Y[i])):
+        breed_column = unique_sorted_Y[j]
+        submission.loc[i, breed_column] = test_Y[i, j]
+
+# 8.40 submission 데이터 확인
+print(submission.iloc[:5, :5])
+
+# 8.41 submission 데이터프레임을 csv 파일로 저장
+submission.to_csv('dogbreed_submission_inceptionV3_epoch10_299.csv', index=False)

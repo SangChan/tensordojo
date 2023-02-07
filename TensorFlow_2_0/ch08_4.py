f2ad09c8d14bb6ec73a1108f423ef19318dff208
Sangchan <sangchan82@gmail.com>
2023-02-07 19:13:28 +0900

    add new'

--- TensorFlow_2_0/ch08_4.py
+++ TensorFlow_2_0/ch08_4.py
@@ -23,13 +23,13 @@ content_batch = tf.expand_dims(content_batch, axis=0)
 
 content_layers = ['block5_conv2']
 
+# 8.44 VGG-19 네트워크 불러오기
 from tensorflow.keras.applications import VGG19
 from tensorflow.keras.applications.vgg19 import preprocess_input
 
 vgg = VGG19(include_top=False, weights='imagenet')
 vgg.trainable = False
 outputs = [vgg.get_layer(name).output for name in content_layers]
-model = tf.keras.Model([vgg.input], outputs)
 model_content = tf.keras.Model([vgg.input], outputs)
 content_output = model_content(preprocess_input(content_batch * 255.0))
 
@@ -49,18 +49,28 @@ total_variation_weight = 1e9
 style_weight = 1e-2
 content_weight = 1e4
 
+# 8.46 Gram matrix 계산 함수 정의
+def gram_matrix(input_tensor):
+    channels = int(input_tensor.shape[-1])
+    a = tf.reshape(input_tensor, [-1, channels])
+    n = tf.shape(a)[0]
+    gram = tf.matmul(a, a, transpose_a=True)
+    return gram / tf.cast(n, tf.float32)
+
+# 8.50 타겟 텍스쳐를 업데이트하기 위한 함수 정의
 def get_outputs(image):
     image_batch = tf.expand_dims(image, axis=0)
-    output = model(preprocess_input(image_batch * 255.0))
-    return [gram_matrix(out) for out in output]
+    output = model_content(preprocess_input(image_batch * 255.0))
+    outputs = [gram_matrix(out) for out in output]
+    return outputs
 
 @tf.function()
 def train_step(image):
     with tf.GradientTape() as tape:
-        _outputs = get_outputs(image)
-        _output2 = get_content_output(image)
-        loss = style_weight * get_loss(_outputs, style_outputs)
-        loss += content_weight * get_content_loss(_output2, content_output)
+        outputs = get_outputs(image)
+        output2 = get_content_output(image)
+        loss = style_weight * get_loss(outputs, style_outputs)
+        loss += content_weight * get_content_loss(output2, content_output)
         loss += total_variation_weight * total_variation_loss(image)
 
     grad = tape.gradient(loss, image)

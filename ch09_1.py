2859c423d092b365a5be19821fddf18b0c4b697f
Sangchan <sangchan82@gmail.com>
2023-02-09 19:00:07 +0900

    add new

--- ch09_1.py
+++ ch09_1.py
@@ -15,3 +15,39 @@ plt.colorbar()
 plt.show()
 
 print(train_Y[0])
+
+# 9.3 Dense 오토인코더 모델 정의
+train_X = train_X.reshape(-1, 28 * 28)
+test_X = test_X.reshape(-1, 28 * 28)
+print(train_X.shape, train_Y.shape)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(784, activation='relu', input_shape=(784,)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(784, activation='sigmoid')
+])
+
+model.compile(optimizer=tf.optimizers.Adam(), loss='mse')
+model.summary()
+
+# 9.4 Dense 오토인코더 모델 학습
+model.fit(train_X, train_X, epochs=10, batch_size=256)
+
+# 9.5 test 데이터로 Dense 오토인코더의 이미지 재생성
+import random
+
+plt.figure(figsize=(4,8))
+for c in range(4):
+    plt.subplot(4, 2, c*2+1)
+    rand_index = random.randint(0, test_X.shape[0])
+    plt.imshow(test_X[rand_index].reshape(28, 28), cmap='gray')
+    plt.axis('off')
+    
+    plt.subplot(4, 2, c*2+2)
+    img = model.predict(np.expand_dims(test_X[rand_index], axis=0))
+    plt.imshow(img.reshape(28, 28), cmap='gray')
+    plt.axis('off')
+    
+plt.show()
+
+model.evaluate(test_X, test_X)

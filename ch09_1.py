143bc9ce8387e861e51903d9ec7c142753e33312
Sangchan <sangchan82@gmail.com>
2023-02-10 19:07:33 +0900

    add new

--- ch09_1.py
+++ ch09_1.py
@@ -51,3 +51,95 @@ for c in range(4):
 plt.show()
 
 model.evaluate(test_X, test_X)
+
+# 9.6 컨볼루션 오토인코더 모델 정의
+train_X = train_X.reshape(-1, 28, 28, 1)
+test_X = test_X.reshape(-1, 28, 28, 1)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(filters=32, kernel_size=2, strides=(2,2), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.Conv2D(filters=64, kernel_size=2, strides=(2,2), activation='relu'),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(7*7*64, activation='relu'),
+    tf.keras.layers.Reshape(target_shape=(7,7,64)),
+    tf.keras.layers.Conv2DTranspose(filters=32, kernel_size=2, strides=(2,2), padding='same', activation='relu'),
+    tf.keras.layers.Conv2DTranspose(filters=1, kernel_size=2, strides=(2,2), padding='same', activation='sigmoid')
+])
+
+model.compile(optimizer=tf.optimizers.Adam(), loss='mse')
+model.summary()
+
+# 9.7 컨볼루션 오토인코더 모델 학습
+model.fit(train_X, train_X, epochs=20, batch_size=256)
+
+# 9.8 test 데이터로 컨볼루션 오토인코더의 이미지 재생성
+import random
+
+plt.figure(figsize=(4,8))
+for c in range(4):
+    plt.subplot(4, 2, c*2+1)
+    rand_index = random.randint(0, test_X.shape[0])
+    plt.imshow(test_X[rand_index].reshape(28, 28), cmap='gray')
+    plt.axis('off')
+    
+    plt.subplot(4, 2, c*2+2)
+    img = model.predict(np.expand_dims(test_X[rand_index], axis=0))
+    plt.imshow(img.reshape(28, 28), cmap='gray')
+    plt.axis('off')
+    
+plt.show()
+
+model.evaluate(test_X, test_X)
+
+# 그림 9.7 출력 코드
+import math
+
+x = np.arange(-5, 5, 0.01)
+relu = [0 if z < 0 else z for z in x]
+elu = [1.0 * (np.exp(z) - 1) if z < 0 else z for z in x]
+
+# plt.axhline(0, color='gray')
+plt.axvline(0, color='gray')
+plt.plot(x, relu, 'r--', label='relu')
+plt.plot(x, elu, 'g-', label='elu')
+plt.legend()
+plt.show()
+
+# 9.9 활성화함수를 elu로 바꾼 컨볼루션 오토인코더 모델 정의 및 학습
+train_X = train_X.reshape(-1, 28, 28, 1)
+test_X = test_X.reshape(-1, 28, 28, 1)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(filters=32, kernel_size=2, strides=(2,2), activation='elu', input_shape=(28, 28, 1)),
+    tf.keras.layers.Conv2D(filters=64, kernel_size=2, strides=(2,2), activation='elu'),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation='elu'),
+    tf.keras.layers.Dense(7*7*64, activation='elu'),
+    tf.keras.layers.Reshape(target_shape=(7,7,64)),
+    tf.keras.layers.Conv2DTranspose(filters=32, kernel_size=2, strides=(2,2), padding='same', activation='elu'),
+    tf.keras.layers.Conv2DTranspose(filters=1, kernel_size=2, strides=(2,2), padding='same', activation='sigmoid')
+])
+
+model.compile(optimizer=tf.optimizers.Adam(), loss='mse')
+
+model.fit(train_X, train_X, epochs=20, batch_size=256)
+
+# 9.10 test 데이터로 컨볼루션 오토인코더의 이미지 재생성
+import random
+
+plt.figure(figsize=(4,8))
+for c in range(4):
+    plt.subplot(4, 2, c*2+1)
+    rand_index = random.randint(0, test_X.shape[0])
+    plt.imshow(test_X[rand_index].reshape(28, 28), cmap='gray')
+    plt.axis('off')
+    
+    plt.subplot(4, 2, c*2+2)
+    img = model.predict(np.expand_dims(test_X[rand_index], axis=0))
+    plt.imshow(img.reshape(28, 28), cmap='gray')
+    plt.axis('off')
+    
+plt.show()
+
+model.evaluate(test_X, test_X)

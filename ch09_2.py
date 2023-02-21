760a98237ebab5571aad10d33da28fdd21e99707
Sangchan <sangchan82@gmail.com>
2023-02-21 19:48:43 +0900

    add new

--- ch09_2.py
+++ ch09_2.py
@@ -23,3 +23,100 @@ for c in range(9):
     plt.title(all_image_paths[c])
     plt.axis('off')
 plt.show()
+
+# 9.21 이미지 경로 분리 저장
+train_path, valid_path, test_path = [], [], []
+
+for image_path in all_image_paths:
+    if str(image_path).split('.')[-1] != 'jpg':
+        continue
+    
+    if str(image_path).split('/')[-2] == 'train':
+        train_path.append(str(image_path))
+    elif str(image_path).split('/')[-2] == 'val':
+        valid_path.append(str(image_path))
+    else:
+        test_path.append(str(image_path))
+
+# 9.22 원본 이미지에서 조각을 추출하고 입력, 출력 데이터를 반환하는 함수 정의
+def get_hr_and_lr(image_path):
+    img = tf.io.read_file(image_path)
+    img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+
+    hr = tf.image.random_crop(img, [50, 50, 3])
+    lr = tf.image.resize(hr, [25, 25])
+    lr = tf.image.resize(lr, [50, 50])
+    return lr, hr
+
+# 9.23 train, valid Dataset 정의
+train_dataset = tf.data.Dataset.list_files(train_path)
+train_dataset = train_dataset.map(get_hr_and_lr)
+train_dataset = train_dataset.repeat()
+train_dataset = train_dataset.batch(16)
+
+valid_dataset = tf.data.Dataset.list_files(valid_path)
+valid_dataset = valid_dataset.map(get_hr_and_lr)
+valid_dataset = valid_dataset.repeat()
+valid_dataset = valid_dataset.batch(1)
+
+def REDNet(num_layers):
+    conv_layers = []
+    deconv_layers = []
+    residual_layers = []
+
+    inputs = tf.keras.layers.Input(shape=(None, None, 3))
+    conv_layers.append(tf.keras.layers.Conv2D(3, kernel_size=3, padding='same', activation='relu'))
+
+    for i in range(num_layers-1):
+        conv_layers.append(tf.keras.layers.Conv2D(64, kernel_size=3, padding='same', activation='relu'))
+        deconv_layers.append(tf.keras.layers.Conv2DTranspose(64, kernel_size=3, padding='same', activation='relu'))
+
+    deconv_layers.append(tf.keras.layers.Conv2DTranspose(3, kernel_size=3, padding='same'))
+
+    # 인코더 시작
+    x = conv_layers[0](inputs)
+
+    for i in range(num_layers-1):
+        x = conv_layers[i+1](x)
+        if i % 2 == 0:
+            residual_layers.append(x)
+
+    # 디코더 시작
+    for i in range(num_layers-1):
+        if i % 2 == 1:
+            x = tf.keras.layers.Add()([x, residual_layers.pop()])
+            x = tf.keras.layers.Activation('relu')(x)
+        x = deconv_layers[i](x) 
+
+    x = deconv_layers[-1](x)
+    
+    model = tf.keras.Model(inputs=inputs, outputs=x)
+    return model
+
+# 9.25 PSNR 정의
+def psnr_metric(y_true, y_pred):
+    return tf.image.psnr(y_true, y_pred, max_val=1.0)
+
+# 9.26 REDNet-30 네트워크 초기화 및 컴파일
+model = REDNet(15)
+model.compile(optimizer=tf.optimizers.Adam(0.0001), loss='mse', metrics=[psnr_metric])
+# 네트워크 시각화
+# tf.keras.utils.plot_model(model)
+
+# 9.27 REDNet-30 네트워크 학습
+history = model.fit_generator(train_dataset, 
+                              epochs=1000, 
+                              steps_per_epoch=len(train_path)//16, 
+                              validation_data=valid_dataset, 
+                              validation_steps=len(valid_path), 
+                              verbose=2)
+
+# 9.28 REDNet-30 네트워크 학습 결과 확인
+import matplotlib.pyplot as plt
+plt.plot(history.history['psnr_metric'], 'b-', label='psnr')
+plt.plot(history.history['val_psnr_metric'], 'r--', label='val_psnr')
+plt.xlabel('Epoch')
+plt.legend()
+plt.show()
+     
